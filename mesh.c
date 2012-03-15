c0444bbaa220180f01c243e90e8c2eea270e2a32
Ashok Nagarajan <ashok@cozybit.com>
2012-03-15 12:06:59 -0700

    iw: add mesh conf parameter "mesh_sync_offset_max_neighor"
    Signed-off-by: Marco Porsch <marco.porsch@etit.tu-chemnitz.de>

--- mesh.c
+++ mesh.c
@@ -130,6 +130,11 @@ static void _print_u16_in_TUs(struct nlattr *a)
 	printf("%d TUs", nla_get_u16(a));
 }
 
+static void _print_u32(struct nlattr *a)
+{
+	printf("%d", nla_get_u32(a));
+}
+
 static void _print_u32_timeout(struct nlattr *a)
 {
 	printf("%u milliseconds", nla_get_u32(a));
@@ -173,6 +178,9 @@ const static struct mesh_param_descr _mesh_param_descrs[] =
 	{"mesh_auto_open_plinks",
 	NL80211_MESHCONF_AUTO_OPEN_PLINKS,
 	_my_nla_put_u8, _parse_u8_as_bool, _print_u8},
+	{"mesh_sync_offset_max_neighor",
+	NL80211_MESHCONF_SYNC_OFFSET_MAX_NEIGHBOR,
+	_my_nla_put_u32, _parse_u32, _print_u32},
 	{"mesh_hwmp_max_preq_retries",
 	NL80211_MESHCONF_HWMP_MAX_PREQ_RETRIES,
 	_my_nla_put_u8, _parse_u8, _print_u8},
