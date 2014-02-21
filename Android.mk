d724128ee5b871092eb7c8c802b1c9f3899d3031
Jason Mobarak <jam@cozybit.com>
2014-02-21 15:26:39 -0800

    iw: fix build of for Android
    Change-Id: Ifbbabe29844b9e463b755f9f7edc69cffaa4f3a1

--- Android.mk
+++ Android.mk
@@ -3,8 +3,13 @@ IW_SOURCE_DIR := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
 
-NO_PKG_CONFIG=y
-include $(LOCAL_PATH)/Makefile
+OBJS = iw.o genl.o event.o info.o phy.o \
+	interface.o ibss.o station.o survey.o util.o \
+	mesh.o mpath.o scan.o reg.o version.o \
+	reason.o status.o connect.o link.o offch.o ps.o cqm.o \
+	bitrate.o wowlan.o coalesce.o roc.o p2p.o hwsim.o
+
+OBJS += sections.o
 
 LOCAL_SRC_FILES := $(patsubst %.o,%.c,$(OBJS)) android-nl.c
 
@@ -14,9 +19,9 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_CFLAGS += -DCONFIG_LIBNL20
 LOCAL_LDFLAGS := -Wl,--no-gc-sections
-#LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_TAGS := eng
 LOCAL_SHARED_LIBRARIES := libnl_2
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE := iw
 
 $(IW_SOURCE_DIR)/version.c:
